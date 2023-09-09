bb8ddf128221c433ebdb3d597a26554e92a56a3a
Stéphane Klein <contact@stephane-klein.info>
2023-09-09 18:29:17 +0200

    WIP

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,7 +1,10 @@
 import postgres from "postgres";
 
 const sql = postgres(
-    process.env.POSTGRES_URL || "postgres://postgres:password@localhost:5432/myapp"
+    process.env.POSTGRES_URL || "postgres://postgres:password@localhost:5432/myapp",
+    {
+        stats: true
+    }
 );
 
 export default sql;
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,12 +1,12 @@
 export async function load({ locals }) {
+    const result = await locals.sql`
+        SELECT
+            users.id AS id,
+            users.username AS username
+        FROM public.users
+    `;
+    console.log(result);
     return {
-        users: (
-            await locals.sql`
-                SELECT
-                    users.id AS id,
-                    users.username AS username
-                FROM public.users
-            `
-        )
+        users: result
     };
 }
